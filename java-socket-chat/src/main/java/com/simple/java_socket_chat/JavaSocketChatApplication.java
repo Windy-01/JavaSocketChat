1544b125064584ce8ad5e3d89aad460f43e70bab
Windy <zq2658636354@163.com>
2025-10-07 13:39:49 +0800

    初始化模板

---
+++ java-socket-chat/src/main/java/com/simple/java_socket_chat/JavaSocketChatApplication.java
@@ -0,0 +1,13 @@
+package com.simple.java_socket_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSocketChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSocketChatApplication.class, args);
+	}
+
+}
---
+++ java-socket-chat/src/test/java/com/simple/java_socket_chat/JavaSocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.java_socket_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSocketChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
