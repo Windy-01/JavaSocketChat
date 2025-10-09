0f3bf51b0881ae3a1978da416adbae6b207d5422
Windy <zq2658636354@163.com>
2025-10-09 21:59:22 +0800

    Nothing

---
+++ src/main/java/com/_4/javasocketchat/JavasocketchatApplication.java
@@ -0,0 +1,13 @@
+package com._4.javasocketchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavasocketchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavasocketchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_4/javasocketchat/JavasocketchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com._4.javasocketchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavasocketchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
