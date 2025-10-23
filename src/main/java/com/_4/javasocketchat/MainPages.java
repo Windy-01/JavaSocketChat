9992663658329b58d8de5d53022ae997cf63d98f
Windy <zq2658636354@163.com>
2025-10-23 15:04:06 +0800

    连接了数据库

--- src/main/java/com/_4/javasocketchat/MainPages.java
+++ src/main/java/com/_4/javasocketchat/MainPages.java
@@ -2,11 +2,19 @@ package com._4.javasocketchat;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainPages {
-    @GetMapping("/home")
-    public String Home(){
+    @GetMapping("/")
+    public String main(){
         return "redirect:/login";
+    }
+
+    @GetMapping("/home/{uid}")
+    @ResponseBody
+    public String home(@PathVariable String uid){
+        return uid;
     }
 }
